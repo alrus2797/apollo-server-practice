5da8c8767c49e9bd49bf78cb08460eaf263b666d
Alexander Apaza <alrus2797@gmail.com>
2023-06-15 12:03:38 -0500

    feat: add graphql codegen

---
+++ src/codegen.ts
@@ -0,0 +1,20 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "src/schema.graphql",
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        useIndexSignature: true,
+        contextType: "../index#ContextValue",
+      },
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"],
+    },
+  },
+};
+
+export default config;
--- src/context-value.ts
+++
@@ -1,7 +0,0 @@
-import { TypicodeAPI } from "./rest/typicode";
-
-export interface ContextValue {
-  dataSources: {
-    commentsAPI: TypicodeAPI;
-  };
-}
