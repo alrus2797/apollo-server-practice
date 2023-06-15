df7fcbdd95d5565a93250e09df2ed1e569a3b80d
Alexander Apaza <alrus2797@gmail.com>
2023-06-15 12:04:12 -0500

    feat: suuport new .graphql file

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,20 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { TypicodeAPI } from "./rest/typicode";
 import { resolvers } from "./resolvers";
-import { ContextValue } from "./context-value";
-import { typeDefs } from "./schema";
 
+import { readFileSync } from "fs";
+
+const typeDefs = readFileSync("./src/schema.graphql", { encoding: "utf-8" });
+
+export interface ContextValue {
+  dataSources: {
+    commentsAPI: TypicodeAPI;
+  };
+}
 
 const server = new ApolloServer<ContextValue>({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
